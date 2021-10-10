c73c56aeb9e65e70099b6a9ccf9c41c257ae5aa4
Yogesh <52989607+yogesh2k21@users.noreply.github.com>
2021-10-10 13:32:39 +0530

    Scroll Indicator is implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,16 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
+/*=============== When the user scrolls the page, execute myFunction==============*/
+window.onscroll = function() {myFunction()};
+            
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
